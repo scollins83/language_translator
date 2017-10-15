720d4ee473c9dd785c2100c9349597e57c4f772a
Sara Collins <datadivallc@gmail.com>
2017-10-14 20:40:11 -0500

    Added passing test and code for saving and re-loading preprocessing data.

--- language_trainer.py
+++ language_trainer.py
@@ -3,6 +3,7 @@ import argparse
 import json
 import sys
 import copy
+import pickle
 
 CODES = {'<PAD>': 0, '<EOS>': 1, '<UNK>': 2, '<GO>': 3}
 
@@ -63,11 +64,12 @@ def create_lookup_tables(text):
 
     return vocab_to_int, int_to_vocab
 
-def text_to_ids(text_list, vocab_to_int, text_type):
+
+def text_to_ids(text_list, vocab_to_int):
     """
     Converts text to vocabulary integers.
-    :param text: Input list of text items.
-    :param text_type: Valid inputs are 'x' and 'y'
+    :param text_list: Input list of text items.
+    :param vocab_to_int: Vocab-to-int dictionary.
     :return: List of integer codes for each word.
     """
     text_ids = [[vocab_to_int.get(word, vocab_to_int['<UNK>'])
@@ -77,6 +79,53 @@ def text_to_ids(text_list, vocab_to_int, text_type):
     return text_ids
 
 
+def add_eos_tag_to_ids(id_text_list, eos_tag):
+    """
+    Adds 'eos' (end-of-sentence) tag to end of each sentence.
+    :param id_text_list: List of id-converted sentences.
+    :param eos_tag: EOS tag value.
+    :return: id_text_list with eos_tag value added to the end of each sentence.
+    """
+    for sentence in id_text_list:
+        sentence.append(eos_tag)
+
+    return id_text_list
+
+
+def save_preprocessing_data(x_id_sequence, y_id_sequence,
+                           x_vocab_to_int, y_vocab_to_int,
+                           x_int_to_vocab, y_int_to_vocab,
+                           preprocess_path):
+    """
+    Saves the x and y parameter objects to a pickle file.
+    :param x_id_sequence:
+    :param y_id_sequence:
+    :param x_vocab_to_int:
+    :param y_vocab_to_int:
+    :param x_int_to_vocab:
+    :param y_int_to_vocab:
+    :param preprocess_path: Path to save the pickle to.
+    :return: Saves the object as a pickle.
+    """
+    with open(preprocess_path, 'wb') as out_file:
+        pickle.dump((
+            (x_id_sequence, y_id_sequence),
+            (x_vocab_to_int, y_vocab_to_int),
+            (x_int_to_vocab, y_int_to_vocab)
+        ), out_file)
+
+    return 1
+
+
+def load_preprocessing_data(preprocess_path):
+    """
+    Loads preprocessing data from existing pickle file.
+    :param preprocess_path: Path of the pre-existing preprocess pickle.
+    :return: x_ids, y_ids, x_vocab-to-int, y_vocab-to-int
+    """
+    with open(preprocess_path, 'rb') as in_file:
+        return pickle.load(in_file)
+
 
 if __name__ == '__main__':
 
@@ -85,16 +134,35 @@ if __name__ == '__main__':
 
     # PREPROCESSING
 
-    # Load the datasets
-    x = read_file(config['x_language_path'])
-    y = read_file(config['y_language_path'])
+    if config["use_existing_preprocessing"] == "False":
 
-    # Convert to lowercase
-    x = lower_text(x)
-    y = lower_text(y)
+        # Load the datasets
+        x = read_file(config['x_language_path'])
+        y = read_file(config['y_language_path'])
 
-    # Create lookup dictionaries
-    x_vocab_to_int, x_int_to_vocab = create_lookup_tables(x)
-    y_vocab_to_int, y_int_to_vocab = create_lookup_tables(y)
+        # Convert to lowercase
+        x = lower_text(x)
+        y = lower_text(y)
+
+        # Create lookup dictionaries
+        x_vocab_to_int, x_int_to_vocab = create_lookup_tables(x)
+        y_vocab_to_int, y_int_to_vocab = create_lookup_tables(y)
+
+        # Convert text to ids
+        x_ids = text_to_ids(x, x_vocab_to_int)
+        y_ids = text_to_ids(y, y_vocab_to_int)
+        y_ids = add_eos_tag_to_ids(y_ids, y_vocab_to_int['<EOS>'])
+
+        # Save the preprocessing objects.
+        save_preprocessing_data(x_ids, y_ids, x_vocab_to_int, y_vocab_to_int,
+                                x_int_to_vocab, y_int_to_vocab,
+                                config['preprocessing_path'])
+
+    # CONSTRUCT NEURAL NETWORK
+    (x_ids, y_ids), (x_vocab_to_int,
+                     y_vocab_to_int), _ = load_preprocessing_data(config['preprocessing_path'])
+
+
+
 
     sys.exit()
